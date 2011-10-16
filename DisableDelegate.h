d28a6cb048eac4ec13c0a748a18c2e9159df806b
Albert Pascual <alpascual@gmail.com>
2011-10-16 16:00:13 -0700

    Upgraded to the pin number to disabled

---
+++ DisableDelegate.h
@@ -0,0 +1,13 @@
+//
+//  DisableDelegate.h
+//  DeviceAlarm
+//
+//  Created by Albert Pascual on 10/16/11.
+//  Copyright (c) 2011 Al. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol DisableDelegate <NSObject>
+
+@end
---
+++ RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  DeviceAlarm
+//
+//  Created by Albert Pascual on 10/15/11.
+//  Copyright (c) 2011 Al. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+@end
